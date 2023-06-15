ca62fe66aa629b960ec3f53c141785b0b0ae72f1
bschmalb <bastian.schmalbach@web.de>
2023-06-15 16:46:20 +0200

    feat: implement new example project correctly

---
+++ example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gettext/flutter_gettext/context_ext.dart';
+import 'package:flutter_gettext/flutter_gettext/gettext_localizations_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+Future<void> main() async {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: const Locale('de'),
+      supportedLocales: const [
+        Locale('de'),
+        Locale('en'),
+      ],
+      localizationsDelegates: [
+        GettextLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: Builder(builder: (context) {
+        return Scaffold(
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  context.t(
+                    'There is {0} apple',
+                    keyPlural: 'There are {0} apples',
+                    pArgs: [1],
+                  ),
+                ),
+                Text(
+                  context.t(
+                    'There is {0} apple',
+                    keyPlural: 'There are {0} apples',
+                    pArgs: [2],
+                  ),
+                ),
+                Text(
+                  context.t(
+                    'You have {message_count} message',
+                    keyPlural: 'You have {message_count} messages',
+                    nArgs: {'message_count': 1},
+                  ),
+                ),
+                Text(
+                  context.t(
+                    'You have {message_count} message',
+                    keyPlural: 'You have {message_count} messages',
+                    nArgs: {'message_count': 3},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
