2910c92fd2d445f4683289f2e02419d725fbeac0
bschmalb <bastian.schmalbach@web.de>
2023-06-16 13:35:12 +0200

    fix: migrate plugin to a package to remove platform agnostic code

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/bastianschmalbach/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bastianschmalbach/stagedates/mobile-app-v2/flutter_gettext"
+export "FLUTTER_APPLICATION_PATH=/Users/bastianschmalbach/development/flutter_gettext"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=1.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
